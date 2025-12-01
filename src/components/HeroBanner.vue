bc54b1536cfa0b76cef54b9fa2310dffea67e411
Jocke <joakim03moller@gmail.com>
2025-12-01 15:02:36 +0100

    Lagt till bakgrundsbild

---
+++ src/components/HeroBanner.vue
@@ -0,0 +1,11 @@
+<template>
+    <section class="relative h-screen flex items-center justify-center overflow-hidden">
+        <img src="../assets/bakgrund.webp" alt="Hero bild" class="w-full h-full object-cover opacity-40">
+    </section>
+
+    <!-- Text overlayen -->
+    <div class="absolute inset-0 flex flex-col justify-center items-center text-white">
+        <h1 class="text-6xl font-bold mb-4">Välkommen till <span class="text-cyan-500">MusicList</span></h1>
+        <p class="text-lg">Utforska och hantera dina favoritlåtar!</p>
+    </div>
+</template>
