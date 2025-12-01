7cbc5f8df578e1be1157002ed7fa0dc871c6d781
Zeydz <39154045+Zeydz@users.noreply.github.com>
2025-12-01 23:48:48 +0100

    Lagt till deleteTrack & addTrack

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -18,3 +18,26 @@ export async function fetchTrackById(id) {
   return response.json();
 }
 
+/* Ta bort en specifik låt */
+export async function deleteTrack(id) {
+  const response = await fetch(VITE_API_URL + `/${id}`, {
+    method: "DELETE",
+  });
+  if (!response.ok) {
+    throw new Error("Något gick fel vid borttagning av låt");
+  }
+   return true;
+}
+
+/* Lägg till en post */
+export async function addTrack(track) {
+  const response = await fetch(VITE_API_URL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(track),
+  });
+  if (!response.ok) {
+    throw new Error("Något gick fel vid tillägg av låt");
+  }
+  return await response.json();
+}
