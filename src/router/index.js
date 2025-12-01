dad14fc91c1bef0f04bd8d27e93f509ad7236580
Jocke <joakim03moller@gmail.com>
2025-12-01 11:55:04 +0100

    Lagt till routing för undersidor

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Tracks from '@/views/Tracks.vue'
+
+// Route definitions
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+  { path: '/tracks', component: Tracks }
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
