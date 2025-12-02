2a6c407d9d7b1fe6287488a5fa9dff151ad2aef5
Zeydz <39154045+Zeydz@users.noreply.github.com>
2025-12-02 12:47:49 +0100

    Lagt till laddningstext vid fetch

--- src/views/Tracks.vue
+++ src/views/Tracks.vue
@@ -4,30 +4,33 @@ import { fetchTrackById, fetchTrack, deleteTrack, addTrack } from '@/services/tr
 
 /* Lagra fetch-information */
 const tracks = ref([]);
-
+const loading = ref(true)
 // Hämta låtar när komponenten monteras
 onMounted(async () => {
     try {
         tracks.value = await fetchTrack();
     } catch (error) {
         console.error("Fel vid hämtning av låtar:", error);
+    } finally {
+        loading.value = false;
     }
 });
 
 </script>
 <template>
-    <section class="p-6 text-white">
-        <h1 class="text-3xl font-bold mb-6">Låtlista</h1>
+    <section class="p-6 text-white flex flex-col max-w-4xl mx-auto">
+        <h1 class="text-3xl font-bold mb-6 text-center">Låtar</h1>
         <ul class="space-y-4">
             <!-- For loop för att printa ut -->
-            <li v-for="track in tracks" :key="track._id"
+            <div v-if="loading" class="text-slate-400">Laddar låtar...</div>
+            <li v-else v-for="track in tracks" :key="track._id"
                 class="p-4 bg-slate-800 rounded-lg shadow-md flex justify-between items-center">
                 <div>
                     <h2 class="text-xl font-semibold">{{ track.title }}</h2>
                     <p class="text-sm text-slate-400">Artist: {{ track.artist }}</p>
                     <p class="text-sm text-slate-400">Släppt: {{ track.yearReleased }}</p>
                     <p class="text-sm text-slate-400">Tillagd i spellista: {{ track.addedToPlaylist }}</p>
-                    <p class="text-sm text-slate-400">Genre: {{ track.genre }}</p>
+                    <p  v-if="track.genre && track.genre.trim() !== ''" class="text-sm text-slate-400">Genre: {{ track.genre }}</p>
                 </div>
                 <!-- Knapp för att ta bort en track -->
                 <button @click="deleteTrack(track._id)"
