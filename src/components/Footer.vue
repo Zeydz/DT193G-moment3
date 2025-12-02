9e1278e9f271fdd832da5beb4d97be33d0b79d69
Zeydz <39154045+Zeydz@users.noreply.github.com>
2025-12-02 16:22:39 +0100

    Lagt till footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+    <footer class="bg-slate-950 text-slate-400 py-6 mt-12">
+        <div class="container mx-auto px-4 flex flex-col sm:flex-row justify-between items-center gap-4">
+
+            <!-- Text -->
+            <p class="text-sm">&copy; 2025 MusikLista. Alla rättigheter reserverade.</p>
+            <div>
+                Skapad av Joakim
+            </div>
+
+        </div>
+    </footer>
+</template>
